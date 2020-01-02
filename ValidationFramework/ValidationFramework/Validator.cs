3b83e2d40543daeb018a391b631b751a1e22458e
Minh Trí <MinhTri@Tris-MacBook-Pro.local>
2020-01-02 16:56:07 +0700

    Create Model
    hoan thanh model

---
+++ ValidationFramework/ValidationFramework/Validator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationFramework.LanguageFactory;
+
+namespace ValidationFramework
+{
+    public abstract class Validator
+    {
+        protected int Code; // Loai code
+
+        public virtual object[] check(string input, LanguageNotification l)
+        {
+            return null;
+        }
+    }
+}
