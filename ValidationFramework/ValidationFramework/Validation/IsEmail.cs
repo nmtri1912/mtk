ebfd5ef146ce864b4f3e8c4a7c0fb6fb8441a8d9
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2020-01-03 20:36:12 +0700

    update func check Email, add class IsPassword

--- ValidationFramework/ValidationFramework/Validation/IsEmail.cs
+++ ValidationFramework/ValidationFramework/Validation/IsEmail.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidationFramework.BasicValidation;
 using ValidationFramework.LanguageFactory;
 
 namespace ValidationFramework.Validation
 {
     public class IsEmail : Validator
     {
+        public IsEmail()
+        {
+            this.Code = 13;
+        }
         public override bool check(string input, LanguageNotification l)
         {
-            return base.check(input, l);
+            if(input.IsEmail())
+            {
+                return true;
+            }
+
+            this.Message = l.getErrorMessage(this.Code);
+            return false;
         }
-         
     }
 }
