898e8f18edf944bc99dd61ed5d899f4ef7b06c9d
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2020-01-03 16:44:12 +0700

    update func check in class IsNotEmpty

--- ValidationFramework/ValidationFramework/Validation/IsNotEmpty.cs
+++ ValidationFramework/ValidationFramework/Validation/IsNotEmpty.cs
@@ -7,11 +7,22 @@ using ValidationFramework.LanguageFactory;
 
 namespace ValidationFramework.Validation
 {
-    public class IsNotNull : Validator
+    public class IsNotEmpty : Validator
     {
+        public IsNotEmpty()
+        {
+            this.Code = 2;
+        }
+
         public override bool check(string input, LanguageNotification l)
         {
-            return base.check(input, l);
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                this.Message = l.getErrorMessage(this.Code);
+                return true;
+            }
+            return false;
         }
     }
 }
