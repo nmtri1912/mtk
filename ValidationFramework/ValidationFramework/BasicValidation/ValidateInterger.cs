cd898d8fe9380ed92a7b4e78c81a29c2c81b9d99
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2020-01-03 17:23:09 +0700

    add override interger dataType, and update func check of Class GreaterThan

---
+++ ValidationFramework/ValidationFramework/BasicValidation/ValidateInterger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationFramework.BasicValidation
+{
+    public static class ValidateInterger
+    {
+        public static bool IsNotZero(this int value)
+        {
+            return (value != 0);
+        }
+
+        public static bool IsEqual(this int value, int compare)
+        {
+            return (value == compare);
+        }
+
+        public static bool IsGreaterThan(this int value, int min)
+        {
+            return (value >= min);
+        }
+
+        public static bool IsLessThan(this int value, int max)
+        {
+            return (value <= max);
+        }
+
+        public static bool IsBetween(this int value, int min, int max)
+        {
+            return (value <= max && value >= min);
+        }
+    }
+}
--- ValidationFramework/ValidationFramework/Validation/GreaterThan.cs
+++ ValidationFramework/ValidationFramework/Validation/GreaterThan.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidationFramework.BasicValidation;
 using ValidationFramework.LanguageFactory;
 
 namespace ValidationFramework.Validation
@@ -12,11 +13,26 @@ namespace ValidationFramework.Validation
         private int index;
         public GreaterThan(int index)
         {
+            this.Code = 3;
             this.index = index;
         }
         public override bool check(string input, LanguageNotification l)
         {
-            return base.check(input, l);
+            try{
+                int a = int.Parse(input);
+                if(a.IsGreaterThan(index))
+                {
+                    return true;
+                }
+
+                this.Message = l.getErrorMessage(this.Code);
+                return false;
+            }
+            catch 
+            {
+                this.Message = l.getErrorMessage(this.Code);
+                return false;
+            }
         }
     }
 }
