284c4f5ae52a838d80f20cc6820101654bbb8eac
Thanh Toai <ltttoai.contact97@gmail.com>
2020-01-03 22:48:46 +0700

    Edit FactoryMethod

--- ValidationFramework/ValidationFramework/LanguageFactory/English.cs
+++ ValidationFramework/ValidationFramework/LanguageFactory/English.cs
@@ -13,37 +13,37 @@ namespace ValidationFramework.LanguageFactory
             switch (code)
             {
                 case 1:
-                    return "This field is not empty";
+                    return "not an empty array";
                 case 2:
-                    return "This field is not Email";
+                    return "is an empty array";
                 case 3:
-                    return "'{0}' cannot be null.";
+                    return "Not greater than";
                 case 4:
-                    return "'{0}' cannot be null or empty.";
+                    return "Not greater than or equal to";
                 case 5:
-                    return "'{0}' cannot be null or whitespace only.";
+                    return "Not less than";
                 case 6:
-                    return "'{0}' cannot be zero.";
+                    return "'Not less than or equal to";
                 case 7:
-                    return "'{0}' is not a valid password. Passwords must be 8 to 30 characters, at least on 1 uppercase letter, at least 1 lowercase letter and at least one number.";
+                    return "Exceeded the allowed characters";
                 case 8:
-                    return "'{0}' must be a at least {1} characters.";
+                    return "Too few characters";
                 case 9:
-                    return "'{0}' must be {1} characters or less.";
+                    return "Out of range";
                 case 10:
-                    return "'{0}' must be exactly {1} characters.";
+                    return "Out of paragraph";
                 case 11:
-                    return "'{0}' must be at least {1} and at most {2} characters.";
+                    return "invalid phone number";
                 case 12:
-                    return "'{0}' does not match the specified criteria.";
+                    return "Invalid Credit Card";
                 case 13:
-                    return "'{0}' does not match the specified criteria.";
+                    return "Invalid email";
                 case 14:
-                    return "'{0}' is not a valid email address.";
+                    return "Cannot be used as password";
                 case 15:
-                    return "'{0}' does not match the provided regular expression.";
+                    return "Not equal to";
                 case 16:
-                    return "'{0}' did not match the specified criteria.";
+                    return "Already by";
                 case 17:
                     return "'{0}' is not a valid date.";
                 case 18:
