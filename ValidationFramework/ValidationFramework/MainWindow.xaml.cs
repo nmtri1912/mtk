be49b23ec4075a90218b0bc3ffc9e73549d043fa
Thanh Toai <ltttoai.contact97@gmail.com>
2020-01-04 19:26:17 +0700

    update mainWindow

--- ValidationFramework/ValidationFramework/MainWindow.xaml.cs
+++ ValidationFramework/ValidationFramework/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ValidationFramework.LanguageFactory;
+using ValidationFramework.TypeShow;
 using ValidationFramework.Validation;
 using ValidationFramework.ValidationExtension;
 
@@ -23,36 +24,154 @@ namespace ValidationFramework
     /// </summary>
     public partial class MainWindow : Window
     {
-        private LanguageFac lFac;
+        private LanguageFac lFac = LanguageFac.getInstance();
         private LanguageNotification lNotify;
-        IsEmail n2;
-        Validator deco;
+        ListValidator lv = new ListValidator();
+        ShowBehavior sb;
+        #region Prototype Check
+        Validator creditCheck;
+        Validator phoneCheck;
+        Validator emailCheck;
+        Validator emailDocComCheck;
+        Validator passwordCheck;
+
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
-            lFac = LanguageFac.getInstance();
+
             lNotify = lFac.getNotificationType("vi");
-            n2 = new IsEmail();
-            deco = new IsEmailExtension(n2);
+            creditCheck = new CreditCard();
+            phoneCheck = new Phone();
+            emailCheck = new IsEmail();
+            emailDocComCheck = new IsEmailExtension(new IsEmail());
+            passwordCheck = new IsPassword();
         }
 
-
-
-        
-
-        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        private void comboValidationLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            MessageBox.Show(deco.Message);
+            if (comboValidationLanguage.SelectedIndex == 0)
+            {
+                lNotify = lFac.getNotificationType("vi");
+            }
+            else
+            {
+                lNotify = lFac.getNotificationType("en");
+            }
         }
 
-
-        private void txt1_LostFocus(object sender, RoutedEventArgs e)
+        private void comboShowType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            error1.Content = "";
-            n2 = new IsEmail();
-            deco = new IsEmailExtension(n2);
-            deco.check(textBox.Text, lNotify);
-            error1.Content = deco.Message;
+            if (comboShowType.SelectedIndex == 0)
+            {
+                sb = new MessBox();
+            }
+            else
+            {
+                sb = new Popup();
+            }
+        }
+
+        private void CreditCard_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //5485660034693048
+            creditCheck = new CreditCard();
+            if (!creditCheck.check(textCreditCardName.Text, lNotify))
+            {
+                labelCreditCard.Content = creditCheck.Message;
+                textCreditCardName.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                labelCreditCard.Content = "";
+                textCreditCardName.BorderBrush = Brushes.LightGreen;
+            }
+        }
+
+        private void PhoneNumber_LostFocus(object sender, RoutedEventArgs e)
+        {
+            phoneCheck = new Phone();
+            if (!phoneCheck.check(textPhoneNumber.Text, lNotify))
+            {
+                labelPhoneNumber.Content = phoneCheck.Message;
+                textPhoneNumber.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                labelPhoneNumber.Content = "";
+                textPhoneNumber.BorderBrush = Brushes.LightGreen;
+            }
+        }
+
+        private void Email_LostFocus(object sender, RoutedEventArgs e)
+        {
+            emailCheck = new IsEmail();
+            if (!emailCheck.check(textEmail.Text, lNotify))
+            {
+                labelEmail.Content = emailCheck.Message;
+                textEmail.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                labelEmail.Content = "";
+                textEmail.BorderBrush = Brushes.LightGreen;
+            }
+        }
+
+        private void EmailDocCom_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+            emailDocComCheck = new IsEmailExtension(new IsEmail());
+            if (!emailDocComCheck.check(textEmailDocCom.Text, lNotify))
+            {
+                labelEmailDocCom.Content = emailDocComCheck.Message;
+                textEmailDocCom.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                labelEmailDocCom.Content = "";
+                textEmailDocCom.BorderBrush = Brushes.LightGreen;
+            }
+        }
+
+        private void Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            passwordCheck = new IsPassword();
+            if (!passwordCheck.check(pwbPassword.Password, lNotify))
+            {
+                labelPassword.Content = passwordCheck.Message;
+                pwbPassword.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                labelPassword.Content = "";
+                pwbPassword.BorderBrush = Brushes.LightGreen;
+            }
+        }
+
+        private void buttonSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            lv = new ListValidator();
+            creditCheck = new CreditCard();
+            phoneCheck = new Phone();
+            emailCheck = new IsEmail();
+            emailDocComCheck = new IsEmailExtension(new IsEmail());
+            passwordCheck = new IsPassword();
+
+            creditCheck.check(textCreditCardName.Text, lNotify);
+            phoneCheck.check(textPhoneNumber.Text, lNotify);
+            emailCheck.check(textEmail.Text, lNotify);
+            emailDocComCheck.check(textEmailDocCom.Text, lNotify);
+            passwordCheck.check(pwbPassword.Password, lNotify);
+
+            lv.AddValidator(creditCheck);
+            lv.AddValidator(phoneCheck);
+            lv.AddValidator(emailCheck);
+            lv.AddValidator(emailDocComCheck);
+            lv.AddValidator(passwordCheck);
+
+            lv.SetShowBehavior(sb);
+            lv.show();
         }
     }
 }
