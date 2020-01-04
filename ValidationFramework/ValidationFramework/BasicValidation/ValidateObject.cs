da2578016bd09a57ad5cf57865679e3a072e871c
Nguyễn Hồng Tới <32115833+hongtoinguyen@users.noreply.github.com>
2020-01-04 15:58:08 +0700

    create validate object

---
+++ ValidationFramework/ValidationFramework/BasicValidation/ValidateObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationFramework.BasicValidation
+{
+    public static class ValidateObject
+    {
+        public static bool IsNotNull(this object value)
+        {
+            return (value != null);
+        }
+        public static bool IsNull(this object value)
+        {
+            return !value.IsNotNull();
+        }
+    }
+}
